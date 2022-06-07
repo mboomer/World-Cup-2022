dd062bf3aba5d82b9f3780ea9b6b388e3febd3fe
Mark Boomer <mark.boomer@9habu.com>
2022-06-07 17:20:13 +0100

    Receive the Predictions and insert predictions to DB, return JSON error/completion message

--- php/save-predictions-to-db.php
+++ php/save-predictions-to-db.php
@@ -3,17 +3,23 @@
     // Include config file
     require_once "../../../.php/inc/db.worldcup.inc.php";
 
-    // DB credentials.
+    // DB credentials as constants
     define('DB_HOST', $servername);
     define('DB_NAME', $db);
     define('DB_USER', $username);
     define('DB_PASS', $password);
 
+    //Receive the RAW data from the fetch POST
+    $predictions = trim(file_get_contents("php://input"));
+
+    // decode into an associative array
+    $json_array = json_decode($predictions, true);
+
     try {
-    // Try and establish database connection.
+    
+        // Try and establish database connection.
         try {
             $dbh = new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-            echo "Connected successfully" . "<br>";
         }
         catch (PDOException $e) {
             exit("Error: " . $e->getMessage());
@@ -25,12 +31,8 @@
                 VALUES 
                     (:UserID, :FixtureID, :HomeScore, :AwayScore, :HomeTeam, :AwayTeam, :ResultID, :Points, :Bonus)";
 
-        echo "Prepare SQL successful" . "<br>";
-
         // prepare the query for the database connection
         $query = $dbh -> prepare($sql);
-
-        echo "Prepare QUERY successful" . "<br>";
 
         // bind the parameters
         $query->bindParam(':UserID',    $userid,        PDO::PARAM_INT);
@@ -43,69 +45,42 @@
         $query->bindParam(':Points',    $points,        PDO::PARAM_INT);
         $query->bindParam(':Bonus',     $bonus,         PDO::PARAM_INT);
 
-        echo "BIND parameters successful" . "<br>";
+        // assign the values to the  place holders
+        $userid     = $elem['UserID'];
+        $fixtureid  = $elem['FixtureID'];
+        $homescore  = $elem['HomeScore'];
+        $awayscore  = $elem['AwayScore'];
+        $hometeamid = $elem['HomeTeamID'];
+        $awayteamid = $elem['AwayTeamID'];
+        $resultid   = $elem['ResultID'];
+        $points     = $elem['Points'];
+        $bonus      = $elem['Bonus'];
 
-        $predictions = '[
-                            {"UserID":1,"FixtureID":"1","HomeScore":"3","AwayScore":"1","HomeTeamID":"1","AwayTeamID":"3","ResultID":1,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"2","HomeScore":"2","AwayScore":"2","HomeTeamID":"2","AwayTeamID":"4","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"9","HomeScore":"1","AwayScore":"3","HomeTeamID":"3","AwayTeamID":"4","ResultID":2,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"10","HomeScore":"3","AwayScore":"1","HomeTeamID":"1","AwayTeamID":"2","ResultID":1,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"17","HomeScore":"2","AwayScore":"2","HomeTeamID":"4","AwayTeamID":"1","ResultID":3,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"18","HomeScore":"1","AwayScore":"3","HomeTeamID":"3","AwayTeamID":"2","ResultID":2,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"3","HomeScore":"3","AwayScore":"1","HomeTeamID":"6","AwayTeamID":"8","ResultID":1,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"4","HomeScore":"2","AwayScore":"2","HomeTeamID":"5","AwayTeamID":"7","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"11","HomeScore":"1","AwayScore":"3","HomeTeamID":"7","AwayTeamID":"8","ResultID":3,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"12","HomeScore":"3","AwayScore":"1","HomeTeamID":"5","AwayTeamID":"6","ResultID":2,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"19","HomeScore":"2","AwayScore":"2","HomeTeamID":"8","AwayTeamID":"5","ResultID":3,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"20","HomeScore":"1","AwayScore":"3","HomeTeamID":"6","AwayTeamID":"7","ResultID":3,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"5","HomeScore":"3","AwayScore":"1","HomeTeamID":"12","AwayTeamID":"11","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"6","HomeScore":"2","AwayScore":"2","HomeTeamID":"9","AwayTeamID":"10","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"13","HomeScore":"1","AwayScore":"2","HomeTeamID":"10","AwayTeamID":"11","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"14","HomeScore":"3","AwayScore":"1","HomeTeamID":"9","AwayTeamID":"12","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"21","HomeScore":"2","AwayScore":"2","HomeTeamID":"11","AwayTeamID":"9","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"22","HomeScore":"1","AwayScore":"3","HomeTeamID":"10","AwayTeamID":"12","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"7","HomeScore":"3","AwayScore":"1","HomeTeamID":"15","AwayTeamID":"16","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"8","HomeScore":"2","AwayScore":"2","HomeTeamID":"13","AwayTeamID":"14","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"15","HomeScore":"1","AwayScore":"3","HomeTeamID":"14","AwayTeamID":"16","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"16","HomeScore":"3","AwayScore":"1","HomeTeamID":"13","AwayTeamID":"15","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"23","HomeScore":"2","AwayScore":"2","HomeTeamID":"16","AwayTeamID":"13","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"24","HomeScore":"1","AwayScore":"3","HomeTeamID":"14","AwayTeamID":"15","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"25","HomeScore":"3","AwayScore":"1","HomeTeamID":"1","AwayTeamID":"12","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"26","HomeScore":"2","AwayScore":"2","HomeTeamID":"2","AwayTeamID":"3","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"27","HomeScore":"1","AwayScore":"3","HomeTeamID":"5","AwayTeamID":"4","ResultID":3,"Points":3,"Bonus":0},
-                            {"UserID":1,"FixtureID":"28","HomeScore":"3","AwayScore":"1","HomeTeamID":"8","AwayTeamID":"5","ResultID":3,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"29","HomeScore":"2","AwayScore":"2","HomeTeamID":"9","AwayTeamID":"2","ResultID":3,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"30","HomeScore":"1","AwayScore":"3","HomeTeamID":"10","AwayTeamID":"1","ResultID":3,"Points":1,"Bonus":0},
-                            {"UserID":1,"FixtureID":"31","HomeScore":"3","AwayScore":"1","HomeTeamID":"14","AwayTeamID":"12","ResultID":3,"Points":1,"Bonus":0}
-                        ]';
+        /**
+            need to have this header in place to make the return of the JSON successful
+        */ 
+        header("Content-Type: application/json");
 
-        $json_array = json_decode($predictions, true);
+        /** 
+            array to be returned to the calling PHP stage 
+        */
+        $msg_arr = array( 'Success' => 'Insert predictons into database SUCCESSFUL', 'Failure' => 'Insert predictons into database FAILED' );
 
+        /** 
+            execute the query and check if it fails to insert
+        */
+        if ($query -> execute() === FALSE) {    
+            echo json_encode( $msg_arr[Failure] );
+            exit;            
+        } 
+    }                       // end of ForEach loop
 
-        foreach($json_array as $elem)  {
+    /**
+        Successful completion 
+    */
+        echo json_encode( $msg_arr[Success] );
 
-            // assign the values to the  place holders
-            $userid     = $elem['UserID'];
-            $fixtureid  = $elem['FixtureID'];
-            $homescore  = $elem['HomeScore'];
-            $awayscore  = $elem['AwayScore'];
-            $hometeamid = $elem['HomeTeamID'];
-            $awayteamid = $elem['AwayTeamID'];
-            $resultid   = $elem['ResultID'];
-            $points     = $elem['Points'];
-            $bonus      = $elem['Bonus'];
-
-            echo "ASSIGN VALUES successful" . "<br>";
-
-            // execute the query
-            if ($query -> execute() === FALSE) {
-                echo 'Unable to insert data - ' . $elem['FixtureID'] ;
-            } else {
-                echo "EXECUTE QUERY successful - " . $elem['FixtureID'] . "<br>";
-            }
-
-        }                       // end of foreach loop
-    }                           // end of Try
+    }  // end of Try
     catch (PDOException $e) {
         echo 'Database Error ' . $e->getMessage() . ' in ' . $e->getFile() . ': '.$e->getLine();
     };
