43983bcbf932cd2cae60345fdcfe926fdb3b685e
Mark Boomer <mark.boomer@9habu.com>
2022-08-23 13:45:23 +0100

    Updated user profile with header and footer

---
+++ include/header4.inc.php
@@ -0,0 +1,33 @@
+                <div id="logo">
+                    <img src='../img/logo.png' alt='World Cup Fortune Teller logo'>
+                </div>
+
+                <div id="qatar-flag-l">
+                    <img src='../img/flags/Qatar.png' alt='Qatar Country Flag'>
+                </div>
+
+                <div id="header-text">
+                    <h1>Qatar 2022 - Predictor</h1> 
+                </div>
+          
+                <div id="username">
+                    User: <?php echo $username ?>
+                </div>
+                
+                <div id="qatar-flag-r">
+                    <img src='../img/flags/Qatar-rev.png' alt='Qatar Country Flag'>
+                </div>
+
+                <div id="hb-icon" class="dropdown">
+
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3">
+                        <div class="dropdown-content">
+                            <a href="user-profile.php">User Profile</a>
+                            <a href="logout.php">Logout</a>
+                        </div>
+                    </div>
+
+                </div>
+
